ad0a0c79b02944284dec27c035ac9f4f864a04a6
VeraAverina <veraverous@>
2020-12-27 23:05:13 +0300

    27_12

---
+++ app/jobs/notify_subscribers_job.rb
@@ -0,0 +1,11 @@
+class NotifySubscribersJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    text = args[0]
+    Subscriber.all do |subscribe|
+      TelegramService.instance.send_message(text, subscribe.chat_name)
+    end
+  end
+  NotifySubscribersJob.set(wait_until: Date.tomorrow.noon).perform_later(message)
+end
